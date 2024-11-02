342a7ae37cd99adbe689a17f84328a12484ad408
C0CNCKA <c0cnckamail@gmail.com>
2024-11-03 01:32:19 +0300

    Add files via upload

---
+++ Елочка-2.py
@@ -0,0 +1,10 @@
+import time
+h = int(input("Введите высоту: "))
+s = 'I'
+
+for m in range(0,h):
+    for n in range(m,h):
+        print(" ",end="")
+    for i in range(h-(2*m+1),h):
+        print(s, end="")
+    print("")
